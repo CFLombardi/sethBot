76bfeffe3df133e1e67cca258eb5d8399bab3f9d
Anthony Tyrpak <Sanzman12@yahoo.com>
2017-07-22 02:46:48 -0400

    Adding anti-spam feature, currently has an infinite loop from time to time that I can't figure out

--- commands/dosh.js
+++ commands/dosh.js
@@ -13,28 +13,6 @@ exports.run = function(msg, currentDosh) {
   var target;
   var vote;
 
-/* I need to figure out where to put this
-
-  //A user can only adjust dosh one a target once per 5 minutes
-  for(var i = 0; i < messageHistory.length; i++) {
-      if(
-         (msg.author.username === messageHistory[i].user) &&
-         (command[0].toLowerCase() === messageHistory[i].target.toLowerCase()) &&
-         ((timeStamp - messageHistory[i].timeStamp) < 300000)
-        ) {
-          msg.channel.send("Bro, you've already voted.  GET THAT SHIT OUT OF HERE!");
-          return false;
-      }
-  }
-
-  messageHistory.push({
-    userID: msg.author.id,
-    target: command[0],
-    timeStamp: timeStamp
-  });
-*/
-  //console.log("hi");
-
   //Determine whether it's an upvote or a downvote
   if(content[1].endsWith("++")) {
     command = content[1].split("++");
@@ -51,26 +29,54 @@ exports.run = function(msg, currentDosh) {
   //make sure the command has valid targets
   target = validateTargets(msg, target);
 
-  if(target != false || target.length === 0) {
-    mentions.forEach(function(value, key, mentions) {
-      if(value.bot) {
-        msg.channel.send("What are you thinking bro?!  Don't feed the bots.");
-      } else {
-        var user = currentDosh.get(key);
-        if(user === undefined) {
-          user = new User(value.id, value.username);
-          currentDosh.set(value.id, user);
+  if(target != false) {
+    //A user can only adjust dosh on a target once per 5 minutes
+    for(var i = 0; i < messageHistory.length; i++) {
+      if(msg.author.id === messageHistory[i].userID) {
+        if((msg.createdTimestamp - messageHistory[i].timeStamp) < 300000) {
+          for(var value of mentions) {
+            if(value[1].id === messageHistory[i].target) {
+              msg.channel.send("Bro, you've already voted.  GET THAT SHIT OUT OF HERE!");
+              return false;
+            }
+          }
+          //There's an infinite loop that happens here
+          //can't seem to figure out why
+          console.log(messageHistory.length);
+          console.log(messageHistory);
+          for(var i = 0; i < target.length; i++) {
+            if(target[i] != undefined && target[i].toLowerCase().slice(1) === messageHistory[i].target.toLowerCase()) {
+              msg.channel.send("Bro, you've already voted.  GET THAT SHIT OUT OF HERE!");
+              return false;
+            }
+          }
         }
-        (vote === "+") ? user.addDosh() : user.removeDosh();
       }
-    });
+    }
+
+    if(mentions != null) {
+      mentions.forEach(function(value, key, mentions) {
+        if(value.bot) {
+          msg.channel.send("What are you thinking bro?!  Don't feed the bots.");
+        } else {
+          var user = currentDosh.get(key);
+          if(user === undefined) {
+            user = new User(value.id, value.username);
+            currentDosh.set(value.id, user);
+          }
+          (vote === "+") ? user.addDosh() : user.removeDosh();
+          messageHistory.push({
+            userID: msg.author.id,
+            target: key,
+            timeStamp: msg.createdTimestamp
+          });
+        }
+      });
+    }
 
     for(var i = 0; i < target.length; i++) {
       var found;
-
-      if(target[i].startsWith("@")) {
-        target[i] = target[i].slice(1);
-      }
+      target[i] = target[i].slice(1);
 
       currentDosh.forEach(function(value, key, users) {
         //if so update their dosh
@@ -86,6 +92,11 @@ exports.run = function(msg, currentDosh) {
         (vote === "+") ? user.addDosh() : user.removeDosh();
         currentDosh.set(msg.id+i, user);
       }
+      messageHistory.push({
+        userID: msg.author.id,
+        target: target[i],
+        timeStamp: msg.createdTimestamp
+      });
     }
   } else {
     return target;
@@ -96,27 +107,41 @@ exports.run = function(msg, currentDosh) {
 
 //Take in the input from the user and message to reply if necessary
 function validateTargets(message, input) {
+  var targets = input.split(" ");
   var isValid;
-
-  badChars.forEach(function(char) {
-    if(input.includes(char)) {
-      message.channel.send("Now you're just making up letters.  Take your 'special' characters and get out! GET OUT!!");
-      isValid = false;
-    }
-  });
 
   if(input === "") {
     message.channel.send("Sir.  Sir!  SIR!  You need to pick a valid target");
     isValid = false;
+    return isValid;
+  }
+
+  for(var char of badChars) {
+    if(input.includes(char)) {
+      message.channel.send("Now you're just making up letters.  Take your 'special' characters and get out! GET OUT!!");
+      isValid = false;
+      return isValid;
+    }
+  }
+
+  for(var i = 0; i < targets.length; i++) {
+    if(!targets[i].includes("@")) {
+      message.channel.send("Ah, ah, ah.  You didn't say the magic word");
+      isValid = false;
+      return isValid;
+    }
   }
 
   if(isValid != false) {
-    var targets = input.split(" ");
     var i = targets.length;
     while(i--) {
       if(targets[i].startsWith("<@")) {
         targets.splice(i, 1);
       }
+    }
+
+    if(targets.length === 0) {
+      targets = true;
     }
     return targets;
   } else {
