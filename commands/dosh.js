1ea8ab3e636e25324d7baf7d736efd0a56c99435
Kydane <baumgardnerk12@gmail.com>
2017-12-02 13:18:44 -0500

    Bring dosh logic into dosh command

--- commands/dosh.js
+++ commands/dosh.js
@@ -1,10 +1,47 @@
 const User = require("../GJUser.js");
+const fs = require("fs");
+require("collections/shim-array");
+require("collections/listen/array-changes");
+
 //this will track users who have invoked this command
 //for each message we will track the userID, the target, and time invoked
 var messageHistory = [];
 var badChars = ["@", "+", "-", "*", "/", "\\", "\"", "\'", "$", "(", ")", "[", "]", "{", "}", ":"]
 
-exports.run = function(msg, currentDosh) {
+//Key: Discord User ID ::: Value: Discord
+const karmaMap = new Map();
+
+//Tries to load a savedCountJson... if that doesn't exist, it will throw an error but thats fine.. it wont hurt.
+fs.readFile('savedCount.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data); //now it an object
+    for(var i in obj.dosh)
+    {
+      var id = obj.dosh[i].id;
+      var name = obj.dosh[i].name;
+      var totalCount = obj.dosh[i].totalCount;
+      var user = new User(id,name);
+      user.setCount(totalCount);
+      karmaMap.set(id, user);
+    }
+
+
+}});
+
+//Key: messageid ::: Value: collection<integer> (Discord UserIDs of members who reacted to a message )
+const usersWhoReacted = new Map();
+
+//Key: messageid ::: Value: Discord UserID of the user who sent a particular message.
+const messageToUser = new Map();
+
+exports.messageFired = function(config,msg) {
+  sendToCollector(config, msg);
+}
+
+
+exports.run = function(config, msg) {
   var content = msg.content.split("!dosh");
   var mentions = msg.mentions.users;
   var command;
@@ -24,7 +61,7 @@ exports.run = function(msg, currentDosh) {
     var target = validateTargets(msg, content[1].trim());
     var outStr;
     if(target != false && target.length === 1) {
-      currentDosh.forEach(function(value, key, dosh) {
+      karmaMap.forEach(function(value, key, dosh) {
         if(isNaN(target[0])) {
           if(value.name.toLowerCase() === target[0].toLowerCase()) {
             outStr = value.name+" has "+value.getCount()+" dosh, brah!\n";
@@ -49,19 +86,22 @@ exports.run = function(msg, currentDosh) {
     } else if(target.length > 1) {
       msg.channel.send("One at a time broseph.");
     }
-    return false;
+    return;
+
+
   }
 
   //You can't vote on yourself or bots
   for (var value of mentions) {
     if(value[0] === msg.author.id) {
       msg.channel.send("I respect the self love bro, but you can't vote for yourself.  Bad form!  SHAME!");
-      return false;
+      
+      return;
     }
 
     if(value[1].bot) {
       msg.channel.send("What are you thinking bro?!  Don't feed the bots.");
-      return false;
+      return;
     }
   }
 
@@ -73,7 +113,7 @@ exports.run = function(msg, currentDosh) {
   if(targets != false) {
     var isValid = checkHistory(msg, targets);
   } else {
-    return false;
+    return;
   }
 
   if(isValid) {
@@ -82,13 +122,13 @@ exports.run = function(msg, currentDosh) {
       var user;
 
       if(isTargetUN) {
-        user = checkMapForTarget(targets[i], currentDosh, isTargetUN);
+        user = checkMapForTarget(targets[i], karmaMap, isTargetUN);
         if(user === undefined) {
           user = new User(msg.id+i, targets[i]);
         }
       } else {
         user = checkMapForTarget(targets[i], mentions, isTargetUN);
-        var tempUser = checkMapForTarget(user.id, currentDosh, isTargetUN);
+        var tempUser = checkMapForTarget(user.id, karmaMap, isTargetUN);
 
         if(tempUser === undefined) {
           user = new User(user.id, user.username);
@@ -98,7 +138,7 @@ exports.run = function(msg, currentDosh) {
       }
 
       (vote === "+") ? user.addDosh() : user.removeDosh();
-      currentDosh.set(user.getID(), user);
+      karmaMap.set(user.getID(), user);
 
       messageHistory.push({
         userID: msg.author.id,
@@ -108,10 +148,21 @@ exports.run = function(msg, currentDosh) {
 
     }//for loop targets.length
     msg.channel.send("You got it brah!");
-    return true;
+    save();
   } //if isValid
 
+
 }//this is the end of the export
+
+function sendToCollector(config, msg){
+    const collector = msg.createReactionCollector(
+    (reaction, user) => (reaction.emoji.id==config.downEmoji || reaction.emoji.id==config.upEmoji) && !user.bot,
+    { time: 43200000 }//12 hours for collection time before it dies.
+     //{ time: 10000 }//10 seconds for collection time before it dies.
+
+    );
+    trackCollector(msg, collector,config);
+}
 
 //A user can only adjust dosh on a target once per 30 minutes.
 //Returns true if they haven't voted for the target yet
@@ -202,5 +253,112 @@ function validateTargets(message, input) {
   } //end of for targets.length
 
   return theTargets;
+
+}
+
+//Saves the Karma Map.
+function save() {
+  var obj = {
+     dosh: []
+  };
+  karmaMap.forEach( function(value,key,karmaMap) {
+  obj.dosh.push(value);
+  });
+  var json = JSON.stringify(obj);
+  fs.writeFile('savedCount.json', json, 'utf8', null);
+}
+
+//Takes in a collector and a message and sets up the tracker.
+function trackCollector(msg, collector,config)
+{
+  collector.on('collect', r =>
+  {
+    if(r.emoji.id == config.upEmoji)
+    {
+      handleUp(r);
+    }
+    if(r.emoji.id == config.downEmoji)
+    {
+      handleDown(r);
+    }
+  });
+
+  collector.on('end', collected =>
+  {
+    collected.forEach(function(value,key,collected)
+    {
+      messageToUser.delete(value.message.id);
+      usersWhoReacted.delete(value.message.id);
+    });
+
+  });
+}
+
+//Handle when Dosh goes up
+function handleUp(reaction)
+{
+  updateDosh(reaction,true);
+}
+//Handle when goes down.
+function handleDown(reaction)
+{
+  updateDosh(reaction,false);
+}
+
+//General function to handle the physical raising and lowering of dosh.
+function updateDosh(reaction,addDosh)
+{
+  var messageUsers = reaction.users;
+  var msg = reaction.message;
+  var authorID = msg.author.id;
+  var authorName = msg.author.username;
+
+  //If this is the first time we are seeing a message, add it to the maps that keep track of this stuff.
+  if(!messageToUser.has(msg.id))
+  {
+    messageToUser.set(msg.id,authorID);
+    usersWhoReacted.set(msg.id,new Array());
+  }
+
+  //For each user who ever commented, go through and make sure they have the appropriate +1 or -1
+  messageUsers.forEach(function(value,key,messageUsers)
+  {
+    //If the user is one of these users who up/downvoted themselves, yell at them please.
+    if(key == authorID)
+    {
+      msg.reply("Bruh, voting your own post? Get out. I SAID GET THAT SHIT OUT");
+      console.log("Someone is trying to upvote their own post... jerks.");
+      reaction.remove(msg.author);
+      return;
+    }
+
+    //If the user already has a vote for or against, then clear it.
+    var reactedUsers = usersWhoReacted.get(msg.id);
+    if(reactedUsers.has(key))
+    {
+      console.log("this person already has a vote.. no can do");
+    }
+    else
+    {
+      reactedUsers.push(key);
+      var user = karmaMap.get(authorID);
+      if(user == undefined)
+      {
+        console.log("User Undefined... adding to list of known users");
+        user = new User(authorID, authorName);
+        karmaMap.set(authorID, user);
+      }
+      if(addDosh)
+      {
+        user.addDosh();
+      }
+      else
+      {
+        user.removeDosh();
+      }
+    }
+    //Save the JSON.
+    save();
+  });
 
 }
