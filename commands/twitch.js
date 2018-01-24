ae24f6184cc816ae04237f161f2d30327618dfe2
Kydane <baumgardnerk12@gmail.com>
2018-01-23 22:50:15 -0500

    Nearing the final commit
    Added a bunch of comments.

--- commands/twitch.js
+++ commands/twitch.js
@@ -4,16 +4,25 @@ const fs = require("fs");
 const bodyParser = require('body-parser');
 var CronJob = require('cron').CronJob;
 var url = require('url');
-var randomBoolean = require('random-boolean');
 var express = require('express');
-var parseString = require('xml2js').parseString;
 require("collections/shim-array");
 require("collections/listen/array-changes");
 
 //Key: Twitch User ID ::: Value: Discord Name
+// This map contains all the users that have been successfully validated to have a twitch id and are currently being 
+// subscribed to to post messages about.
 const userIDMap = new Map();
 
+/*
+	channels = an array that holds all the discord channels we are sending notifications to.
+	seenIDs = an array that holds all the Twitch Message IDs we recieve since sometimes they send duplicates.
+	permission = an array of roles a user could have that will allow the user to run "admin" commands.
+
+	clientID, clientSecret = these is used to tell Twitch, "Hey. I'm a bot and this is the application using this information." These credentials can be obtained at: https://dev.twitch.tv/
+	host/port/path = where we tell twitch to send its notifications to.
+*/
 var channels, 
+	seenIDs = [],
 	permission,
 	clientID,
 	clientSecret,
@@ -21,39 +30,49 @@ var channels,
 	port,
 	path;
 
+//Tells if we have been inited.
+//Because at this time both "!live" and "!discord" are bound to this file. I only want "init" to be run once since it will be called twice.
 var inited = false;
 
+//This is the discord bot. Its needed to lookup channels from ids.
 var seth;
 
+//Runs everytime a message is recieved.
 exports.run = function(config, msg) {
+	//cuts messages on whitepace.
 	var command = msg.content.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } );
-	//we can assume command[0] is useless.
+	//we can assume command[0] is useless, command[1] has the command and command [2] has information about a command..
+	//COMMAND TO ADD USERS
 	if(command[1].toLowerCase() == "add"){
 		if(!checkPermission(msg)){
 			return;
 		}
+		//Get the username, if its not defined, tell the user that.
 		var username = command[2];
 		if(typeof username == "undefined"){
 			msg.channel.send("Gotta include a name. !twitch add [twitchusername]");
 			return;
 		}
+		//Create an array and all a subscription. If its successful, tell the user.
 		var name = [];
 		name.push(username);
 		setUpSubscription(name, "subscribe", function(){
 			msg.channel.send("Successfully paired "+username+"!");
 		});
-
 	}
+	//COMMAND TO REMOVE USERS
 	else if(command[1].toLowerCase() == "remove"){
 		if(!checkPermission(msg)){
 			return;
 		}
+		//Get the username, if its undefined, tell the user.
 		var username = command[2];
 		var foundInMap = false;
 		if(typeof username == "undefined"){
 			msg.channel.send("Gotta include a name. !twitch add [twitchusername]");
 			return;
 		}
+		//If we already know the user isn't in the map, we can ignore the request and yell at the user for being a moron.
 	  	userIDMap.forEach( function(value,key,userIDMap) {
 	  		if(value.name == username){
 	  			foundInMap = true;
@@ -63,13 +82,16 @@ exports.run = function(config, msg) {
 	  		msg.channel.send(username+" wasn't being watched.");
 			return;
 	  	}
+	  	//Try an send an unsubscribe, if its successful. notify the user.
 		var name = [];
 		name.push(username);
 		setUpSubscription(name, "unsubscribe", function(){
 			msg.channel.send("Successfully unpaired "+username+"!");
 		});
 	}
+	//COMMAND TO REMOVE USERS
 	else if(command[1].toLowerCase() == "list"){
+		//anyone can run this command. All we need to go is go through the user map and pull out the names. if there are no users, tell them that.
 		var values = [];
 	  	userIDMap.forEach( function(value,key,userIDMap) {
 	  		values.push(value.name);
@@ -79,10 +101,13 @@ exports.run = function(config, msg) {
 			return;
 		}
 		msg.channel.send("I'm watching: \n"+values.join("\n"));
-	}else if(command[1].toLowerCase() == "listen"){
+	}
+	//COMMAND TO SEND NOTIFICATIONS TO A DISCORD CHANNEL.
+	else if(command[1].toLowerCase() == "listen"){
 		if(!checkPermission(msg)){
 			return;
 		}
+		//Look at the channels array, if it doesn't include the channel id that we are looking at... add it.
 		var id = msg.channel.id;
 		if(!channels.includes(id)){
 			channels.push(id);
@@ -92,10 +117,13 @@ exports.run = function(config, msg) {
 		}
 		msg.channel.send("I'M SCREAMING ALREADY");
 
-	}else if(command[1].toLowerCase() == "unlisten"){
+	}
+	//COMMAND TO STOP SENDING NOTIFICATIONS TO A DISCORD CHANNEL.
+	else if(command[1].toLowerCase() == "unlisten"){
 		if(!checkPermission(msg)){
 			return;
 		}
+		//Look at the channels array and remove the channel id if we find it.
 		var id = msg.channel.id;
 		var index = channels.indexOf(id);
 		if(typeof index != 'undefined' && index > -1){
@@ -112,33 +140,45 @@ exports.run = function(config, msg) {
 
 }
 
+//This is fired anytime a message is sent whether it says !Command or not. For twitch, we don't care.
 exports.messageFired = function(config,msg) {
 	//do nothing  
 }
 
+//This is fired at the start up and always run.
 exports.init = function(bot, config){
+	//Because at this time both "!live" and "!discord" are bound to this file. I only want "init" to be run once since it will be called twice.
 	if(inited)return;
 	inited = true;
 
+	//jsons are stored in the twitch directory, This ensures that the directory is made
 	var dir = './twitch';
 	if (!fs.existsSync(dir)){
 	    fs.mkdirSync(dir);
 	}
+	//This saves the bot for later use while looking up channels.
 	seth = bot;
+	//This pulls in all the discord channel ids of where to post the notifications.
 	channels = readChannels();
-	//get the Client ID and Client Secret
+	//get the Client ID and Client Secret 
 	clientID = config.twitch.twitch_client_id;
 	clientSecret = config.twitch.twitch_client_secret;
+	//get the host, port, and path of where the POST/GET from twitch should be sent 
 	host = config.twitch.twitch_callback_host;
 	port = config.twitch.twitch_callback_port;
 	path = config.twitch.twitch_callback_path;
+	//This pulls in all the permission groups from the config that should have access to the "admin" commands.
 	permission = config.twitch.permission_groups;
 
-
+	//This is where we listen for GET and POST calls from Twitch.
+	//It opens up the same path for GET and POST.
+	//The way webhooks work, the POST endpoint is used anytime twitch wants to notify us. 
+	//The GET endpoint is used when we "subscribe" to a user. It expects us to return a challenge hash to tell twitch "This is a real address and i'm willing to accept GET/POSTS"
 	console.log("Twitch Webhook callback initializing...");
 	var app = express();
 	app.use(bodyParser.json());
 	app.get('/seth/twitch', function(req, res) { 
+		//Parse the GET request, if it contains "hub.challenge" return that so Twitch will send us messages. Otherwise just give it a 200 (OK) response code.
 		var url_parts = url.parse(req.url, true);
 		var challenge = url_parts.query['hub.challenge'];
 		res.writeHead(200);
@@ -148,21 +188,33 @@ exports.init = function(bot, config){
 		}
 		res.end();
 	});
+
 	app.post('/seth/twitch', function(req, res) {
+		//Parse the POST request, we know its going to have JSON in the body.
+		//If that JSON has a "data" element, we send the messages to the discord channels.
+		//Twitch might accidently send multiple of the same request. for that reason, there is a safe guard in place to only show one notification to the users. (Array seenIDs)
 		var url_parts = url.parse(req.url, true);
 		var data = req.body.data;
 		if(typeof data[0] == "undefined") {
 			console.log("something just ended... we can ignore it.");
 		}else{
-			postNotification(data[0]);
+			if(seenIDs.includes(data[0].id)){
+				postNotification(data[0]);
+				seenIDs.push(data[0].id);	
+			}
 		}
 			
 	  res.sendStatus(200);
 	});
+	//Turn on the GET and POST calls.
 	app.listen(port);
 	console.log('Listening on port '+port+'...');
 
+	//For all the existing twitch ids we know (which are stored in TwitchSubs.json) re-up their subscriptions
 	updateAll();
+
+	//This is a cron job. 
+	//This cron job says at 2:00am EST every single day to re-up subscriptions. This is important because subscriptions are disabled every 10 days so we need to keep them fresh.
 	new CronJob('0 0 2 * * * ', function() {
 		console.log("updating twitch");
 		updateAll();
@@ -170,8 +222,10 @@ exports.init = function(bot, config){
 	
 }
 
+//This clears all the users we know about, gets all the subscriptions from twitch/TwitchSubs.json and then sets up new subscriptions for all of them.
 function updateAll(){
 	userIDMap.clear();
+	seenIDs = [];
 	var subs = getSubs();
 	var names = [];
 	for(var i in subs){
@@ -182,8 +236,10 @@ function updateAll(){
 }
 
 function setUpSubscription(names, subVal, pair){
+	//If there are no names to make subscriptions for, end it right here.
 	if(typeof names == "undefined" || names.length == 0 ) return;
 
+	//So we have a bunch of names, we need to translate those names into twitch ids. this first call does that.
 	var options = {
 		host: "api.twitch.tv",
 		port: 443,
@@ -198,19 +254,24 @@ function setUpSubscription(names, subVal, pair){
 		res.setEncoding('utf8');
 		res.on('data', function (chunk) {
 			var ret = JSON.parse(chunk);
+			//ret now contains all of the users that successfully have twitch ids. if a names was given to us that is invalid, we dropped it on the floor.
 			var users = ret.users;
 			for(var i in users){
 				var user = users[i];
+				//if we are subscribing, add the user information to the userIDMap
+				//if we are unsubscribing a user, remove that infomation.
 				if(subVal == "subscribe"){
 					userIDMap.set(user._id, user);
 				}
 				else if (subVal == "unsubscribe"){
 					userIDMap.delete(user._id);
 				}
-				
+				//Send the subscription webhook request
 				subscribe(subVal, user._id, pair);
 			}
+			//At this point, let people know we found a twitch id and we successfully were able to send a subscription hook.
 			if(typeof pair == "function") pair();
+			//write the userIDMap to disk with all the users we had success with.
 			save();
 
   		});
@@ -220,13 +281,16 @@ function setUpSubscription(names, subVal, pair){
 }
 
 function postNotification(data){
+	//for each channel that was subscribed to, send a notification about an up stream.
 	for(var i in channels){
-		seth.channels.get(channels[i]).send("!!! TWITCH NOTIFICATION: "+userIDMap.get(data.user_id).display_name+" has gone live. !!!");	
+		seth.channels.get(channels[i]).send("!!! TWITCH NOTIFICATION: "+userIDMap.get(data.user_id).display_name+" has gone live. ===> https://www.twitch.tv/"+userIDMap.get(data.user_id).name+" !!!");	
 	}
 
 }
 
 function subscribe(subVal, userid, pair){
+	//Using the twitch API, send the webhook request. 
+	//This will be picked up in the GET request we saw in the "init" method after twitch messages us back.
 	var options = {
 		host: "api.twitch.tv",
 		port: 443,
@@ -273,11 +337,13 @@ function save() {
   fs.writeFile('twitch/TwitchSubs.json', json, 'utf8', null);
 }
 
+//Saves the active channels to write to , to disk.
 function saveChannels(){
   var json = JSON.stringify(channels);
   fs.writeFile('twitch/channels.json', json, 'utf8', null);
 }
 
+//Gets all of the Twitch Subs saved to disk.
 function getSubs(){  
   //Tries to load a savedCountJson... if that doesn't exist, it will throw an error but thats fine.. it wont hurt.
   try{ 
@@ -292,6 +358,7 @@ function getSubs(){
 
 }
 
+//Reads all the channels saved to disk.
 function readChannels(){
    //Tries to load a savedCountJson... if that doesn't exist, it will throw an error but thats fine.. it wont hurt.
    try{
@@ -305,6 +372,7 @@ function readChannels(){
   return JSON.parse(file); //now it an object
 }
 
+//This makes sure a user has the roles specified in the config to run the admin based commands.
 function checkPermission(msg){
 	var hasRole = false;
 	for(var i in permission){
