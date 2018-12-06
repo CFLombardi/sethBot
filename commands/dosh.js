3ff6147011ebda37213565823f3c01c632430952
Kyle Baumgardner <baumgardnerk12@gmail.com>
2018-12-05 21:16:03 -0500

    Removing unneeded var

--- commands/dosh.js
+++ commands/dosh.js
@@ -7,7 +7,6 @@ require("collections/listen/array-changes");
 //for each message we will track the userID, the target, and time invoked
 var messageHistory = [];
 var badChars = ["@", "+", "-", "*", "/", "\\", "\"", "\'", "$", "(", ")", "[", "]", "{", "}", ":"];
-var prefix = "!dosh";
 
 //Key: Discord User ID ::: Value: Discord
 const karmaMap = new Map();
