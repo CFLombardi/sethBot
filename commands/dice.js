d556d0527d8a1b5620511ddaffced8050f19bf55
Anthony <sanzman12@yahoo.com>
2019-02-14 05:28:42 +0000

    Assigned user input to variable

--- commands/dice.js
+++ commands/dice.js
@@ -3,14 +3,15 @@ var Roll = require('roll');
 exports.run = function(config, msg) {
   var roll = new Roll();
   var valid = roll.validate(msg.content);
-
+  var input = msg.content;
   if(valid) {
-    userRoll = roll.roll(msg.content);
+
+    userRoll = roll.roll(input);
   } else {
     msg.channel.send("That dice command is not in the proper format.");
     return;
   }
-  
+
   msg.channel.send("Here are the dice you rolled: ["+userRoll.rolled+"]\nAnd here is the sum of those rolls: "+userRoll.result);
 }
 
